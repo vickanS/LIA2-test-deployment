5d4073f223a2b727bd11757d2e3a8a5dc1ad98a9
vickanS <stennvictoria@gmail.com>
2025-06-04 11:35:50 +0200

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import "../styles/home.css"
+
+function Home() {
+    return (
+        <div className="content">
+                <h1>Tack för denhär tiden!</h1>
+        </div>
+    )
+  }
+  
+  export default Home;
